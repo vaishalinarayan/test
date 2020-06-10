4cb8c9eecc62df81cbe4b9eeae629830f5dca14c
vaibhav5788 <66732598+vaibhav5788@users.noreply.github.com>
2020-06-10 18:22:18 +0530

    Add files via upload

---
+++ brightcove-video.js
@@ -0,0 +1,58 @@
+function handlePlaybackEvent_(event) {
+  var state = event.type;
+  var player = this;
+
+  var eventDetail = {
+    currentTime: player.currentTime(),
+    duration: player.duration(),
+    name: player.mediainfo.name,
+    state: state,
+  };
+  var eventInit = {
+    detail: eventDetail,
+  };
+
+  console.log('Player is available');
+  var customEvent = new CustomEvent(state, eventInit);
+  console.log(customEvent);
+  window.aa = customEvent;
+  console.log("file 2");
+  player.dispatchEvent(customEvent);
+
+}
+
+function handleBrightcovePlayers(numTries) {
+  var players = videojs.getPlayers();
+  var playerIds = Object.keys(players);
+  if (playerIds.length === 0) {
+    // players are not ready, try again
+    // give up after about 7.5 seconds
+    if (numTries < 10) {
+      // use exponential backoff to delay
+      setTimeout(function() {
+        var numTries = numTries * 2;
+        handleBrightcovePlayers(numTries);
+      }, numTries * 500);
+    }
+  } else {
+    try {
+      videojs.getPlayer('J5eBQvT9f').ready(function() {
+        var player = this;
+        var playerEvents = [
+          'ended',
+          'pause',
+          'play',
+        ];
+        playerEvents.forEach(function(playerEvent) {
+			console.log(playerEvent);
+		  console.log("playerEvent");
+          player.on(playerEvent, handlePlaybackEvent_);
+        });
+      });
+    } catch (e) {
+      console.warn(e);
+    }
+  }
+}
+console.log("**Start**")
+handleBrightcovePlayers(1);
