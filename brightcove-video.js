271492a4cebb649b19800c374e64aec4e84b018c
vaibhav5788 <66732598+vaibhav5788@users.noreply.github.com>
2020-06-12 23:44:14 +0530

    Update brightcove-video.js

--- brightcove-video.js
+++ brightcove-video.js
@@ -1,26 +1,3 @@
-/**
- * @fileoverview Detects video playback events in the Brightcove player and
- * sends a CustomEvent for each of them.
- *
- * Although the player is loaded in an IFRAME, Brightcover overwrites
- * `postMessage` with their own function, so you can't post messages between
- * the parent and frame windows.
- *
- * But the script is run in the context of the parent window. So CustomEvents
- * can be dispatched and they can be received in the main window.
- *
- *@doc Brightcove Player development overview https://player.support.brightcove.com/coding-topics/overview-player-api.html
- * @doc Brightcove Player API https://docs.brightcove.com/brightcove-player/current-release/Player.html
- * @doc HTML5 media events https://html.spec.whatwg.org/#mediaevents
- */
-
-/**
- * @private
- * Given a playback event, get other metadata related to the event,
- * then send the event's details to the parent.
- * @param {Event} event The playback event.
- * @this The Brightcove player object.
- */
 function handlePlaybackEvent_(event) {
   var state = event.type;
   var player = this;
