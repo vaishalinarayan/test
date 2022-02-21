274444b03cdea8e569ad7f3cc047ed4e63dba6f4
ShatadruMaity <63687156+ShatadruMaity@users.noreply.github.com>
2022-02-21 21:26:23 +0530

    Update brightcove-video.js

--- brightcove-video.js
+++ brightcove-video.js
@@ -2,7 +2,7 @@
  * @fileoverview Detects video playback events in the Brightcove player and
  * sends a CustomEvent for each of them.
  *
- * Although the player is loaded in an IFRAME, Brightcover overwrites
+ * Although the player is loaded in an IFRAME, Brightcove overwrites
  * `postMessage` with their own function, so you can't post messages between
  * the parent and frame windows.
  *
@@ -21,78 +21,72 @@
  * @param {Event} event The playback event.
  * @this The Brightcove player object.
  */
-function handlePlaybackEvent_(event) {
-  var state = event.type;
-  var player = this;
 
-  var eventDetail = {
-    currentTime: player.currentTime(),
-    duration: player.duration(),
-    name: player.mediainfo.name,
-    state: state,
-	id: player.mediainfo.id,
-  };
-  var eventInit = {
-    detail: eventDetail,
-  };
+function handlePlaybackEvent_(event)
+{
+	var state = event.type;
+	var player = this;
+	var eventDetail = {
+    		currentTime: player.currentTime(),
+   duration: player.duration(),
+   name: player.mediainfo.name,
+   state: state,
+		id: player.mediainfo.id,
+	};
+  	var eventInit = {
+    		detail: eventDetail,
+  	};
   
-  
-	
-	var fcurrentTime = player.currentTime();
+  	var fcurrentTime = player.currentTime();
 	var fduration = player.duration();
 	var fpercentViewed = Math.floor((fcurrentTime/fduration)*100);
 	
-	
-	if (state=="timeupdate" && Math.floor(fpercentViewed / 25)*25==0&& getCookie("pstate")!="0")
-		
-		{
+/* tracking video milestones – start, 25, 50, 75, and end */
+
+	if (state=="timeupdate" && fpercentViewed == 1)
+	{
 		document.cookie = "pstate=0"
-		  window.parent.postMessage(eventDetail,"*");
-		}	
+		window.parent.postMessage(eventDetail,"*");
+	}	
 	
 	if (state=="timeupdate" && Math.floor(fpercentViewed / 25)*25==25&& getCookie("pstate")!="25")
-		
-		{
+	{
 		document.cookie = "pstate=25"
-		  window.parent.postMessage(eventDetail,"*");
-		  
-		}	
+		window.parent.postMessage(eventDetail,"*");
+	}	
 	
 	if (state=="timeupdate" && Math.floor(fpercentViewed / 25)*25==50&& getCookie("pstate")!="50")
-		
-		{
+	{
 		document.cookie = "pstate=50"
-		  window.parent.postMessage(eventDetail,"*");		  
-		}
+		window.parent.postMessage(eventDetail,"*");		  
+	}
+
 	if (state=="timeupdate" && Math.floor(fpercentViewed / 25)*25==75&& getCookie("pstate")!="75")
-		
-		{
+	{
 		document.cookie = "pstate=75"
-		  window.parent.postMessage(eventDetail,"*");
-		  
-		}
+		window.parent.postMessage(eventDetail,"*");
+	}
 	
 	if (state=="ended" && getCookie("pstate")!="ended")
-		
-		{
+	{
 		document.cookie = "pstate=ended"
-		  window.parent.postMessage(eventDetail,"*");		  
-		}
+		window.parent.postMessage(eventDetail,"*");		  
+	}
 	
 	function getCookie(cname) {
-	  var name = cname + "=";
-	  var decodedCookie = decodeURIComponent(document.cookie);
-	  var ca = decodedCookie.split(';');
-	  for(var i = 0; i <ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) == ' ') {
-		  c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0) {
-		  return c.substring(name.length, c.length);
-		}
-	  }
-	  return "";
+	  	var name = cname + "=";
+	  	var decodedCookie = decodeURIComponent(document.cookie);
+	  	var ca = decodedCookie.split(';');
+	  	for(var i = 0; i <ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') {
+		  		c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+		  		return c.substring(name.length, c.length);
+			}
+	  	}
+	  	return "";
 	}
 
 }
@@ -102,38 +96,44 @@ function handlePlaybackEvent_(event) {
  * Send CustomEvents to the parent for specific media playback events.
  * @param {int} numTries Counter of tries to check that players are valid.
  */
+
 function handleBrightcovePlayers(numTries) {
-  var players = videojs.getPlayers();
-  var playerIds = Object.keys(players);
-  if (playerIds.length === 0) {
+  	var players = videojs.getPlayers();
+  	var playerIds = Object.keys(players);
+  	
+
+
+	if (playerIds.length === 0) {
     // players are not ready, try again
     // give up after about 7.5 seconds
-    if (numTries < 10) {
-      // use exponential backoff to delay
-      setTimeout(function() {
-        var numTries = numTries * 2;
-        handleBrightcovePlayers(numTries);
-      }, numTries * 500);
-    }
-  } else {
-    var playerId = playerIds[0];
-    try {
-      videojs.getPlayer(playerId).ready(function() {
-        var player = this;
-        var playerEvents = [
-          'ended',
-		  'timeupdate',
-        ];
-        playerEvents.forEach(function(playerEvent) {
-          player.on(playerEvent, handlePlaybackEvent_);
-        });
-      });
-    } catch (e) {
-      console.warn(e);
-    }
-  }
+    	if (numTries < 10) {
+      	// use exponential backoff to delay
+      		setTimeout(function() {
+        		var numTries = numTries * 2;
+        		handleBrightcovePlayers(numTries);
+      		}, numTries * 500);
+    	}
+  	} 
+	else {
+    		var playerId = playerIds[0];
+    		try {
+      			videojs.getPlayer(playerId).ready(function() {
+        			var player = this;
+        			var playerEvents = [
+          					'ended',
+		  				'timeupdate',
+        					];
+        			playerEvents.forEach(function(playerEvent) {
+         			player.on(playerEvent, handlePlaybackEvent_);
+        			});
+      			});
+    		} 
+		catch (e) {
+      			console.warn(e);
+    		}
+  	}
 }
-console.log("brightcove test");
-console.log("***Test****")
+
 document.cookie = "pstate="
 handleBrightcovePlayers(1);
+
